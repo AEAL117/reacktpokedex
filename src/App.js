efb6074655f3897324332e457dee3a99ffe4649b
ALFREDO EZEQUIEL ARENAS LEDESMA <ezequiel_arenas@live.com.mx>
2022-01-14 20:31:21 -0600

    Creacion de Modal Inicia

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState,useEffect} from "react";
+import { useEffect, useState} from "react";
 function App() {
   const [counter,setCounter]=useState(0);
   const [pokemon,setPokemon]=useState({});
+  useEffect(()=> {
+    console.log({pokemon});
+    console.log(counter);
+    
+    
+  },[pokemon,counter]);
 const fetchPokemon =(id)=>{
 fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 .then((response)=> response.json())
 .then((data) => setPokemon(data));
-
+setCounter(id);
   };
 
-  const getRandomInt = (min=1, max=500)=>{
+  const getRandomInt = (min=1, max=898)=>{
     return Math.floor(Math.random()*(max-min)+min)
   };
 
-  useEffect(()=> {
-    console.log({pokemon});
-  },[pokemon]);
+
+
+  const next = ()=>{
+    if(counter>897){
+      fetchPokemon(1);
+      setCounter(1);
+    }else{
+      setCounter((counter) => counter +1);
+      fetchPokemon(counter+1);
+    }
+    
+  };
+  const prev = ()=>{
+   if(counter<2){
+    fetchPokemon(898);
+    setCounter(898);
+   }else{
+    setCounter((counter) => counter -1);
+    fetchPokemon(counter-1);
+   }
+   
+  };
+ const showHabilities=()=>{
+
+ };
   return (
     <div className="App">
       <header className="App-header">
@@ -29,9 +57,10 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
          </div>
          <p>{pokemon.name??"No se ha seleccionado Pokemon"}</p>
          <div className="flax-container">
-           <button  className="button">Back </button>         
-           <button className="button" onClick={()=>fetchPokemon(getRandomInt())}>Random </button>    
-           <button className="button">Next </button>    
+           <button  className="button" onClick={prev}>Anterior</button>         
+           <button className="button" onClick={()=>fetchPokemon(getRandomInt())}>Random</button>    
+           <button className="button" onClick={next}>Siguiente</button>
+           <button className="button" onClick={showHabilities}>Habilidades</button>    
          </div>
         
         </p>
---
+++ src/components/Modals.js
@@ -0,0 +1,7 @@
+const Modals=()=>{
+    return(
+        <div>
+            <h2>Modales</h2>
+        </div>
+    );
+}
