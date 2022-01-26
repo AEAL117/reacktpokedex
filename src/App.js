330a84497febc42e94b2f9b9e79b91f0c9cefd14
ALFREDO EZEQUIEL ARENAS LEDESMA <ezequiel_arenas@live.com.mx>
2022-01-26 11:46:52 -0600

    Use ref implementated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import { useEffect, useState} from "react";
+import { useEffect, useState,useRef} from "react";
 import Modals from './components/Modals';
 import defaultPokemon from './img/default.jpg'
 function App() {
   const [counter,setCounter]=useState(0);
   const [pokemon,setPokemon]=useState({});
+  const inputRef=useRef();
   useEffect(()=> {
     console.log({pokemon});
     console.log(counter);
@@ -15,9 +16,8 @@ const fetchPokemon =(id)=>{
 fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 .then((response)=> response.json())
 .then((data) => setPokemon(data));
-setCounter(id);
-  };
 
+  };
   const getRandomInt = (min=1, max=898)=>{
     return Math.floor(Math.random()*(max-min)+min)
   };
@@ -25,23 +25,35 @@ setCounter(id);
 
 
   const next = ()=>{
-    if(counter>897){
+    if(counter===0){
       fetchPokemon(1);
-      setCounter(1);
+        setCounter(1);
     }else{
-      setCounter((counter) => counter +1);
-      fetchPokemon(counter+1);
+      if(pokemon.id===898){
+        fetchPokemon(1);
+        setCounter(1);
+      }else{
+        setCounter(pokemon.id +1);
+        fetchPokemon(pokemon.id +1);
+      }
     }
+
     
   };
   const prev = ()=>{
-   if(counter<2){
-    fetchPokemon(898);
-    setCounter(898);
-   }else{
-    setCounter((counter) => counter -1);
-    fetchPokemon(counter-1);
-   }
+    if(counter===0){
+      fetchPokemon(898);
+      setCounter(898);
+    }else{
+      if(pokemon.id===1){
+        fetchPokemon(898);
+        setCounter(898);
+       }else{
+        setCounter(pokemon.id -1);
+        fetchPokemon(pokemon.id-1);
+       }
+    }
+
    
   };
   return (
@@ -62,7 +74,10 @@ setCounter(id);
            <button  className="button" onClick={prev}>Anterior</button>         
            <button className="button" onClick={()=>fetchPokemon(getRandomInt())}>Random</button>    
            <button className="button" onClick={next}>Siguiente</button>
-           {counter>0 ?  <Modals poke={pokemon}/>:<h1 className="none">.</h1>}    
+           <h3 className='titulo'>Búsqueda por nombre</h3>
+        <input type="text" onChange={(event) => fetchPokemon(event.target.value)}></input> 
+        
+        {pokemon!=null ?  <Modals poke={pokemon}/>:<h1 className="none">.</h1>}   
          </div>
         
         </p>
